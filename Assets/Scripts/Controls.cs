0ab4fd2ec9ed78f37e8bb589a77ef05cfa800a18
Siddube <prajwal.be.21@gmail.com>
2023-01-23 13:43:31 +0530

    Implement PlayerMovement class, Add Input System

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,289 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Controls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Controls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Controls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Controls"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""88d5a2b4-d61b-4df6-b667-f9782bbb207d"",
+            ""actions"": [
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""b8ce0779-cf5c-4e1a-a92b-44e42e4c0497"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""1557d28b-17ec-4275-91f7-1166067b5127"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""RotateRight"",
+                    ""type"": ""Button"",
+                    ""id"": ""bac9b628-49da-4532-a8ec-673a3aeb7a45"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""RotateLeft"",
+                    ""type"": ""Button"",
+                    ""id"": ""33fa7814-dcd2-4ce8-bf14-cd3c4dc53391"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""6b0c97c8-181c-4326-a142-2017dc777257"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""ff6c9f01-cf8d-4101-9225-04ad96e29845"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""a0d6ddae-c9ae-403c-9e48-942a39b94139"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""82eb35f2-5a91-439b-b8f5-fd1605830434"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""fbf5eae6-785e-452f-b0d5-bfcedee6dcef"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""2572e4b2-83cf-4d73-ac69-c241ff04c161"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""06f15dd8-b2fe-4b5e-833a-08e2d64cc767"",
+                    ""path"": ""<Keyboard>/z"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""RotateRight"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""5d1b116e-10c9-4964-ae9f-102f8a8b3b5a"",
+                    ""path"": ""<Keyboard>/c"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""RotateLeft"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard & Mouse"",
+            ""bindingGroup"": ""Keyboard & Mouse"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
+        m_Player_RotateRight = m_Player.FindAction("RotateRight", throwIfNotFound: true);
+        m_Player_RotateLeft = m_Player.FindAction("RotateLeft", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_Move;
+    private readonly InputAction m_Player_RotateRight;
+    private readonly InputAction m_Player_RotateLeft;
+    public struct PlayerActions
+    {
+        private @Controls m_Wrapper;
+        public PlayerActions(@Controls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputAction @Move => m_Wrapper.m_Player_Move;
+        public InputAction @RotateRight => m_Wrapper.m_Player_RotateRight;
+        public InputAction @RotateLeft => m_Wrapper.m_Player_RotateLeft;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @RotateRight.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRotateRight;
+                @RotateRight.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRotateRight;
+                @RotateRight.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRotateRight;
+                @RotateLeft.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRotateLeft;
+                @RotateLeft.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRotateLeft;
+                @RotateLeft.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRotateLeft;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @RotateRight.started += instance.OnRotateRight;
+                @RotateRight.performed += instance.OnRotateRight;
+                @RotateRight.canceled += instance.OnRotateRight;
+                @RotateLeft.started += instance.OnRotateLeft;
+                @RotateLeft.performed += instance.OnRotateLeft;
+                @RotateLeft.canceled += instance.OnRotateLeft;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    private int m_KeyboardMouseSchemeIndex = -1;
+    public InputControlScheme KeyboardMouseScheme
+    {
+        get
+        {
+            if (m_KeyboardMouseSchemeIndex == -1) m_KeyboardMouseSchemeIndex = asset.FindControlSchemeIndex("Keyboard & Mouse");
+            return asset.controlSchemes[m_KeyboardMouseSchemeIndex];
+        }
+    }
+    public interface IPlayerActions
+    {
+        void OnJump(InputAction.CallbackContext context);
+        void OnMove(InputAction.CallbackContext context);
+        void OnRotateRight(InputAction.CallbackContext context);
+        void OnRotateLeft(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,151 @@
+﻿/* ==========================================================================================================================================
+Author: Prajwal Belagatti
+Class: Player Movement
+Description: Handles player movement
+============================================================================================================================================= */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+  [SerializeField] float sidewardSpeed = 1f;
+  [SerializeField] float jumpSpeed = 1f;
+  [SerializeField] float forwardSpeed = 1f;
+  public Rigidbody rb;
+  private Vector3 sidewardVector = new Vector3(0f, 0f, 0f);
+  private enum MoveForwardDir
+  {
+    up, right, down, left
+  }
+
+  private MoveForwardDir forwardDir = MoveForwardDir.up;
+
+  private void Start()
+  {
+    // Setup rigidbody component
+    SetupRigdbody();
+  }
+
+  private void FixedUpdate()
+  {
+    // Process sidewards movement from Keyboard input
+    ProcessMovement();
+    // Move the player forwards at constant velocity
+    MoveForwards();
+  }
+
+  private void SetupRigdbody()
+  {
+    // Setup rigidbody component to a reference to be used in rest of the class
+    rb = GetComponent<Rigidbody>();
+    // Freeze rotation and handle it through code alone
+    // Ignores Physics
+    rb.freezeRotation = true;
+  }
+
+  public void ProcessMovement()
+  {
+    // Add forces to the left and right movement with sidewardVector variable
+    rb.AddForce(sidewardVector * sidewardSpeed, ForceMode.Acceleration);
+  }
+
+  private void MoveForwards()
+  {
+    // Add constant forward vector
+    // Multiply by -1 as we move in -ve z axis
+    this.gameObject.transform.Translate(Vector3.forward * -1 * forwardSpeed);
+  }
+
+  public void OnJump()
+  {
+    // Jump on pressing Spacebar on keyboard
+    // Return if the player is already jumping
+    // Checks by calculating the player's Y position of the transform 
+    if (this.gameObject.transform.position.y > 1.2) { return; }
+    rb.AddForce(transform.up * jumpSpeed, ForceMode.Impulse);
+  }
+
+  public void OnMove(InputValue input)
+  {
+    // Get a Vector2 value of input controls
+    Vector2 xInput = input.Get<Vector2>();
+    // Set sidewardVector and sidewardDirection based on the player's forward direction
+    SetSidewardDirection(xInput);
+  }
+
+  public void OnRotateRight()
+  {
+    // Rotate player to the right on pressing C
+    // Unfreeze rotation to rotate player
+    rb.freezeRotation = false;
+    // Rotate player
+    this.gameObject.transform.Rotate(0f, -90f, 0f, Space.Self);
+    // Set new forward direction by passing right as parameter to denote player turned right
+    ToggleForwardDirection("right");
+    // Freeze player rotation again
+    rb.freezeRotation = true;
+  }
+  public void OnRotateLeft()
+  {
+    // Rotate player to the left on pressing Z
+    // Unfreeze rotation to rotate player
+    rb.freezeRotation = false;
+    // Rotate player
+    this.gameObject.transform.Rotate(0f, 90f, 0f, Space.Self);
+    // Set new forward direction by passing left as parameter to denote player turned left
+    ToggleForwardDirection("left");
+    // Freeze player rotation again
+    rb.freezeRotation = true;
+  }
+
+
+  /* =======================================================================================================
+  Helper Functions to set forward and sideward direction based on input
+  ========================================================================================================== */
+  private void SetSidewardDirection(Vector2 xInput)
+  {
+    // Set sidewardVector to be used in Process Movement function
+    // If the player is moving up or down from initial rotation
+    // Create vector from input and use it for the x-axis value
+    // Else if left or right create vector from input and use it for the z-axis value
+    if (forwardDir == MoveForwardDir.up)
+      sidewardVector = new Vector3(-xInput.x, 0, 0);
+    else if (forwardDir == MoveForwardDir.down)
+      sidewardVector = new Vector3(xInput.x, 0, 0);
+    else if (forwardDir == MoveForwardDir.right)
+      sidewardVector = new Vector3(0, 0, -xInput.x);
+    else if (forwardDir == MoveForwardDir.left)
+      sidewardVector = new Vector3(0, 0, xInput.x);
+  }
+  private void ToggleForwardDirection(string rotator)
+  {
+    // Check if the function was called from right or left rotation
+    // If right change the forward direction clockwise
+    if (rotator == "right")
+    {
+      if (forwardDir == MoveForwardDir.up)
+        forwardDir = MoveForwardDir.right;
+      else if (forwardDir == MoveForwardDir.right)
+        forwardDir = MoveForwardDir.down;
+      else if (forwardDir == MoveForwardDir.down)
+        forwardDir = MoveForwardDir.left;
+      else if (forwardDir == MoveForwardDir.left)
+        forwardDir = MoveForwardDir.up;
+    }
+    // If left change the forward direction anti-clockwise rotation
+    if (rotator == "left")
+    {
+      if (forwardDir == MoveForwardDir.up)
+        forwardDir = MoveForwardDir.left;
+      else if (forwardDir == MoveForwardDir.left)
+        forwardDir = MoveForwardDir.down;
+      else if (forwardDir == MoveForwardDir.down)
+        forwardDir = MoveForwardDir.right;
+      else if (forwardDir == MoveForwardDir.right)
+        forwardDir = MoveForwardDir.up;
+    }
+  }
+}
